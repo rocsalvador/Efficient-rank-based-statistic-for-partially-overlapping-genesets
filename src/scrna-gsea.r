59d407e0390673966206f04b3b31de7949b92ce8
roc <rocsalvador001@protonmail.com>
2023-05-09 16:13:03 +0200

    Add chunk processing

--- src/scrna-gsea.r
+++ src/scrna-gsea.r
@@ -1,6 +1,24 @@
 library(gseacc)
-library(dplyr)
-library(Seurat)
-library(patchwork)
+# library(Seurat)
+library(anndata)
+# library(patchwork)
 
-gsea2()
+raw <- read_h5ad("data/healthy-raw.h5ad")
+
+geneIds <- raw$var_names
+sampleIds <- raw$obs_names
+gsea <- new(GseaRcpp, sampleIds, geneIds)
+rm(sampleIds)
+rm(geneIds)
+
+nChunks <- 1000
+chunkSize <- as.integer(raw$n_obs / nChunks)
+startSample <- 1
+endSample <- as.integer(chunkSize)
+
+for (i in 1:nChunks) {
+    countMatrix <- raw$chunk_X(select = startSample:endSample)
+    gsea$runChunked(countMatrix)
+    startSample <- startSample + chunkSize
+    endSample <- endSample + chunkSize
+}
